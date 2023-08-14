d9bc1ad837d9d48ae1c3ddfec5ab9fd994a17bd1
Eyasin81 <eyasina81.com>
2023-08-14 14:56:40 +0600

    Convert Array <=>to Object

---
+++ chapter-15/148ObjectFromEntries.js
@@ -0,0 +1,13 @@
+let myObj={
+   a:10,
+   b:20
+}
+// Object To Array Convert
+console.log(Object.entries(myObj));
+// Array To Object Convert
+let myArray=[
+   ["a",10],
+   ["b",20]
+]
+// Array to Object Convert
+console.log(Object.fromEntries(myArray));
