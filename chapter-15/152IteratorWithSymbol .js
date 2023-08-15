5b9f58941a5f4ebc9ef9ec4eb0039d14407362ee
Eyasin81 <eyasina81.com>
2023-08-15 13:44:53 +0600

    Iterator With Symbol

---
+++ chapter-15/152IteratorWithSymbol .js
@@ -0,0 +1,20 @@
+// Array iterator With Symbol Method
+let myArray=[1,2,3]
+let iterator=myArray[Symbol.iterator]()
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+// String iterator With Symbol Method
+let myObj="EyasinAli"
+let iterator1=myObj[Symbol.iterator]()
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+
