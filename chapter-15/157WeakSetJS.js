4a891ffa081122f6e7a1dfd726fc5dd7e61c2100
Eyasin81 <eyasina81.com>
2023-08-17 18:06:27 +0600

    WeakSet

--- chapter-15/157WeakSetJS.js
+++ chapter-15/157WeakSetJS.js
@@ -1,15 +1,21 @@
-// WeakSet has Three property Method :
-//delete 
-//has
-//set 
-//get
-let a=10;b=20
-let weakSet = new WeakSet();
-// Value Set 
-weakSet.add("key1","BD")
-weakSet.add("key2","ID")
-weakSet.add("key3","FB")
- console.log(weakSet);
-//  console.log(weakSet.has(obj));
-//  weakSet.delete(obj);
-//  console.log(weakSet.has(obj));
+// Method 
+//1. add  property.add
+// 2. has() property.has()
+// 3. delete() property.delete()
+const weakSet = new WeakSet();
+
+// Creating objects to store in the WeakSet
+const obj1 = { name: 'Object 1' };
+const obj2 = { name: 'Object 2' };
+// Adding objects to the WeakSet
+weakSet.add(obj1);
+weakSet.add(obj2);
+
+// Checking if an object is in the WeakSet
+console.log(weakSet.has(obj1));  // true
+
+// Removing an object from the WeakSet
+weakSet.delete(obj2);
+
+// Checking again
+console.log(weakSet.has(obj2));  // false
---
+++ chapter-15/all.js
@@ -0,0 +1,15 @@
+class myClass{
+   constructor(name,age){
+      this.name=name
+      this.age=age 
+   }
+   print(){
+      console.log("Hello Programmer ");
+   }
+   print1(){
+    console.log("hello bal");
+   }
+}
+let item=new myClass("Eyasin","24")
+let e="EyasinAli Programmer"
+item.print1(e)
