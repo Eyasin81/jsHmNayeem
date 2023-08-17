1964265a367c984bf1aa544500e5c6eee944bce9
Eyasin81 <eyasina81.com>
2023-08-17 21:46:54 +0600

    Static

--- chapter-15/all.js
+++ chapter-15/all.js
@@ -1,15 +1,47 @@
-class myClass{
-   constructor(name,age){
-      this.name=name
-      this.age=age 
+// Global Context:
+console.log("hello");
+
+console.log(this === window);
+function regularFunction() {
+   console.log(this);
+ }
+ 
+ regularFunction(); // `this` refers to the global object (window in a browser)
+  // true
+  const arrowFunction = () => {
+   console.log(this);
+ };
+ 
+ arrowFunction(); // `this` context depends on where `arrowFunction` was defined
+ const myObject = {
+   myMethod() {
+     console.log(this);
    }
-   print(){
-      console.log("Hello Programmer ");
-   }
-   print1(){
-    console.log("hello bal");
-   }
+ };
+ 
+ myObject.myMethod(); // `this` refers to `myObject`
+//  1. this Inside Global Scope Programize
+let a = this;
+console.log(a);  // Window {}
+
+this.name = 'Sarah';
+console.log(window.name); // Sarah
+// 2. this Inside Function
+function greet() {
+
+   // this inside function
+   // this refers to the global object
+   console.log(this);
 }
-let item=new myClass("Eyasin","24")
-let e="EyasinAli Programmer"
-item.print1(e)
+
+greet(); // Window {}
+// 3. this Inside Constructor Function
+function Person() {
+
+   this.name = 'Jack';
+   console.log(this);
+
+}
+
+let person1 = new Person();
+console.log(person1.name);
