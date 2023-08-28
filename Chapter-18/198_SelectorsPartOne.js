afc3e1bcc36651f529f143eda4c061a81744915b
Eyasin81 <eyasina81.com>
2023-08-28 11:49:25 +0600

    SelectorPartOne

---
+++ Chapter-18/198_SelectorsPartOne.js
@@ -0,0 +1,12 @@
+// id Selector
+let id=document.getElementById('title')
+console.log(id);
+// Class Selector
+let classSelector=document.getElementsByClassName('list-group-item')
+console.log(classSelector);
+// One Tag Selector
+let oneTag=document.getElementsByName('p')
+console.log(oneTag);
+// All Tag Selector
+let allTag=document.getElementsByTagName('p')
+console.log(allTag);
--- Chapter-18/cls-198.js
+++
@@ -1,6 +0,0 @@
-let id=document.getElementById('id_1')
-console.log(id);
-let cla=document.getElementsByClassName('id_1')
-console.log(cla);
-let lists=document.getElementsByTagName('li')
-console.log(lists);
