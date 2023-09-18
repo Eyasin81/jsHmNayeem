b4cc9da5a59f56f12f86d3c5d9e738ad7acd7481
Eyasin81 <eyasina81.com>
2023-09-18 14:18:02 +0600

    search value

---
+++ 046.JS_Search_Value_in_Array.js
@@ -0,0 +1,12 @@
+// Search Value in Array
+var arr=[1,2,3,4,5,6,7,8]
+var f=10;
+for(var i=0;i<arr.length;i++){
+if(arr[i]==f){
+        console.log("Data Found at Index "+ i)
+        break
+    }else{
+        console.log("Data not Found")
+       break 
+    }
+}
