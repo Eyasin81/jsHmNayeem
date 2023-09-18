daa85949b883d8ceb62f3a3281b0ace20e2120f3
Eyasin81 <eyasina81.com>
2023-09-18 12:17:35 +0600

    all js Course

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
-let p =new Promise(function(resolve,reject){
-   let workDone=true; // some time consuming work
-   if(workDone){
-   //invoke resolve function passed
-   resolve('success promise completed')
-   }
-   else{
-   reject('ERROR , work could not be completed')
-   }
-  })
- .then((v)=>{
-   console.log(v);
- })
+// let p =new Promise(function(resolve,reject){
+//    let workDone=true; // some time consuming work
+//    if(workDone){
+//    //invoke resolve function passed
+//    resolve('success promise completed')
+//    }
+//    else{
+//    reject('ERROR , work could not be completed')
+//    }
+//   })
+//  .then((v)=>{
+//    console.log(v);
+//  })
+
+
+
