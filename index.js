35a98474d8b384fb3db9f34aad42f0286509f97e
Eyasin81 <eyasina81.com>
2023-09-19 15:18:40 +0600

    055_ Remove object delete()

---
+++ index.js
@@ -0,0 +1,11 @@
+var person = {
+   firstName: "John",
+   lastName: "Doe",
+   age: 30
+ };
+ 
+ // Remove the 'age' property from the 'person' object
+delete person.lastName
+ 
+ console.log(person);
+ 
