b8f526bc024241882b020f1bef4bbcdea8c2460a
Eyasin81 <eyasina81.com>
2023-09-20 00:42:00 +0600

    059 What is function

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-//object method in js  
-var obj={
-   x:10,
-   y:20
-}
-console.log(Object.keys(obj));
-console.log(Object.values(obj));
-console.log(Object.entries(obj));
-// copy new object 
-let obj1=Object.assign({},obj)
-obj1.xx=30;// new object add item
-console.log(obj1);//output
+function functionName(parameters) {
+   // ফাংশনের কাজের কোড
+   return result; // যেসব ডেটা রিটার্ন করা হবে (optional)
+ }
+ 
