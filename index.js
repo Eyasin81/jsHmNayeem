2c74485d32578fa9fa029438a47eddd79fdb0bad
Eyasin81 <eyasina81.com>
2023-09-23 13:10:38 +0600

    078 find(),findIndex()

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// obj compare
-let  ab={
-  y:10,
-  y:20
-}
-var obj2={
-  x:10,
-  y:20
-}
-console.log(obj1==obj2)//false
+// array reduce Method use 
+var arr=[1,3,5]
+var myArray=arr.reduce((a,b)=>{
+    // return a+b
+    // Maximum Number 
+    // return Math.max(a,b)
+    // Minimum Number 
+    return Math.min(a,b)
+
+    
+})
+console.log(myArray)
