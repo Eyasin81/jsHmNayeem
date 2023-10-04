3c59a033c97b93e1d8f7e56775897d80410c4a68
Eyasin81 <eyasina81.com>
2023-10-05 02:06:09 +0600

    108__Object_Literal

---
+++ index.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isEmployed: true,
+  greet: function() {
+    console.log(`Hello, my name is ${this.firstName} ${this.lastName}.`);
+  }
+};
+
+console.log(person.firstName); // "John"
+console.log(person.age);       // 30
+person.greet();                // "Hello, my name is John Doe."
