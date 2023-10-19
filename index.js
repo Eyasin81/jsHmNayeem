5e879d987a396b12a44752316f9b5c266e996f0b
Eyasin81 <eyasina81.com>
2023-10-19 15:23:50 +0600

    160 Class in Javascript .js

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// Creating a WeakMap
-const weakMap = new WeakMap();
-
-// Creating objects to use as keys
-const key1 = {};
-const key2 = {};
-
-// Adding entries to the WeakMap
-weakMap.set(key1, 'Value 1');
-weakMap.set(key2, 'Value 2');
-
-// Getting values using keys
-console.log(weakMap.get(key1));  // 'Value 1'
-console.log(weakMap.get(key2));  // 'Value 2'
-
-// Deleting an entry
-weakMap.delete(key1);
-
-// Checking again
-console.log(weakMap.get(key1));  // undefined
-// This will throw an error
-for (const [key, value] of weakMap) {
-   console.log(key)
-   console.log(value)
-   console.log(key, value);
- }
- 
+class OurClass{
+  constructor(name,age){
+     this.name=name
+     this.age=age
+  }
+  draw(){
+     console.log(`My Name is ${this.name} & I am ${this.age} Old`)
+  }
+}
+const FullName=new OurClass("Eyasin","23")
+console.log(FullName.age)
+console.log(FullName.name)
+FullName.draw()
