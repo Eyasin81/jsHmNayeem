7564ff41ad7cda34df2fbc4aacdf028ee3b7a52f
Eyasin81 <eyasina81.com>
2023-10-20 07:12:13 +0600

    169 Module System in Js

--- e.js
+++ e.js
@@ -1,4 +1,4 @@
-for (var i = 0; i < 3; i++) {
+/* for (var i = 0; i < 3; i++) {
    // ক্লোজার ফাংশন ডিফাইন করা হয়
    (function (index) {
      setTimeout(function () {
@@ -7,3 +7,11 @@ for (var i = 0; i < 3; i++) {
    })(i); // ফাংশনটি সত্যিকারে কল করা হয়, এবং i এর মান প্যারামিটার হিসেবে পাঠানো হয়
  }
  
+ let e="My name is Eyaisn Ali"
+export default e */
+
+const FulName=()=>{
+console.log("hello I am form e")
+}
+var tiktalk="hello How are you"
+export  default tiktalk
