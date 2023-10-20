e5aebf90100915ef02b6ed403b133f7f8dc89bdd
Eyasin81 <eyasina81.com>
2023-10-20 07:01:39 +0600

    166. Inheritance in ES6 in Javascript

--- 166. Inheritance in ES6 in Javascript.js
+++ 166. Inheritance in ES6 in Javascript.js
@@ -13,4 +13,23 @@ class child extends parent{
 // parent sob golu ei khane ache
 }
 let all=new child()
-all.hello1()
+all.hello1() 
+/* 
+
+// Class Inheritance
+//Method-1
+"use strict"
+class Shape{
+   constructor(name){
+      this.name=name
+   }
+}
+class Sqar extends Shape{
+   ddd(){
+      console.log(`My Name is ${this.name}`)
+   }
+}
+ let e=new Sqar("eyasin ali")
+ e.ddd()
+
+*/
