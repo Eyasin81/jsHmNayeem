6f19d75453e74922abd01817170524f59f04f6cf
Eyasin81 <eyasina81.com>
2023-10-20 07:38:15 +0600

    170 Error in JavaScritp Syntax

---
+++ 170 Error in Javascript.js
@@ -0,0 +1,32 @@
+//Method-1  throw error 
+/* let e=20
+if(e>12){
+throw new Error("Hello I am from Error")
+}else{
+console.log("your are Right ")
+}
+ */
+
+// Method-2  syntax error
+// console.log("hello"
+
+// Method-3 ReferenceError
+// console.log(a)
+
+
+// Syntax: Throwing a generic exception
+
+/* 
+Method-1
+throw new Error("I am Your New Error")
+OR
+throw ("I am Your Error")
+
+*/
+
+// Method-2
+/* Syntax: Throwing a specific exception
+throw new Error_name([message]) 
+
+
+*/
