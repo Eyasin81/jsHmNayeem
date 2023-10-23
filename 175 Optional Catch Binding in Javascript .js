f3d989b70db0c557b5eeecf678a4e8fcbf5b5e17
Eyasin81 <eyasina81.com>
2023-10-23 06:50:50 +0600

    175 Optional Catch Binding in JS

---
+++ 175 Optional Catch Binding in Javascript .js
@@ -0,0 +1,9 @@
+try{
+   console.log("I am Your ne Try ")
+   throw new Error("I am a Error")
+   console.log("I am Your ne Try-1 ")
+   }catch{
+   console.log("Code Run Poblem")
+   }finally{
+      console.log("I am your finally Block")
+   }
