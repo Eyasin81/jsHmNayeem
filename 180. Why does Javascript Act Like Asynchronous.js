c7a9391ee14cfd92e1250b90097bd1d71304a3f6
Eyasin81 <eyasina81.com>
2023-10-23 07:49:00 +0600

    180 Why Does js like Async

---
+++ 180. Why does Javascript Act Like Asynchronous.js
@@ -0,0 +1,7 @@
+/// এই গুলো  একটির পরে একটি কল হবে । 
+let responseA=serverCall(url)
+let responseB=serverCall(url)
+let responseC=serverCall(url)
+console.log(responseA)
+console.log(responseB)
+console.log(responseC)
