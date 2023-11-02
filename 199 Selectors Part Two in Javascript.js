e5866f9f43b0105ccac1da9f41d0390cbb6d9f59
Eyasin81 <eyasina81.com>
2023-11-02 16:53:04 +0600

    DOM Update part-2

---
+++ 199 Selectors Part Two in Javascript.js
@@ -0,0 +1,28 @@
+/* 
+// id selector 
+let id=document.querySelector('#title')
+console.log(id);
+// // class  selector 
+let ClassName=document.querySelector('.list-group-item')
+console.log(ClassName);
+// All  Tag Selector
+let tagName=document.querySelectorAll("p")
+console.log(tagName);
+// //// 
+// console.log(id.constructor.name);
+// console.log(id1.constructor.name);
+
+*/
+//  j-query  selector
+let id=document.querySelector("#title")
+console.log(id)
+// One Class Selector
+let ClassSelector=document.querySelector(".lead")
+console.log(ClassSelector) 
+//All Class Selector 
+let AllClassSelector=document.querySelectorAll(".lead")
+console.log(AllClassSelector)
+// 
+let listItemOne=document.querySelector([name^="list-item-one"]) 
+console.log(listItemOne)
+
