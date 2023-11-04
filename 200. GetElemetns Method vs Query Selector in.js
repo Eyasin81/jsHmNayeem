06bfdaa10d23e26e96505d85fe22bcbbf20274a5
Eyasin81 <eyasina81.com>
2023-11-04 12:02:57 +0600

    Git Element Vs query

---
+++ 200. GetElemetns Method vs Query Selector in.js
@@ -0,0 +1,8 @@
+let li1=document.getElementsByTagName('li')
+console.log('li')
+let li2=document.querySelectorAll('li')
+console.log(li1.constructor.name)
+console.log(li2.constructor.name)
+let ul=document.querySelector('ul')
+console.log(ul.children)
+console.log(ul.childNodes)
--- index.js
+++
@@ -1,8 +0,0 @@
-let li1=document.getElementsByTagName('li')
-console.log('li')
-let li2=document.querySelectorAll('li')
-console.log(li1.constructor.name)
-console.log(li2.constructor.name)
-let ul=document.querySelector('ul')
-console.log(ul.children)
-console.log(ul.childNodes)
