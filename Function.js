385fd90c00a1ea54ae2be2e585dae0fc594cd349
Eyasin81 <eyasina81@gmail.com>
2023-12-31 08:27:10 +0600

    Code test

---
+++ Function.js
@@ -0,0 +1,6 @@
+const r=45
+const t=100
+const add=(r,t)=>{
+   return (r*t)/15
+}
+console.log(add(r,t))
--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-console.log(list.attributes)
-// console.log(list.getAttributes)
-console.log(list.getAttributeNames())
+let postForm=document.getElementById('post-from')
+let posts=document.getElementById('post')
+let URL="https://jsonplaceholder.typicode.com/posts";
+postForm.addEventListener('submit',function(e){
+   e.preventDefault()
+   let title=this.title.value|| undefined
+   let body=this.body.value|| undefined
+   if(title && body){
+      let postObj={
+         userId:100,
+         title,
+         body
+      }
+      fetch(URL,{
+         method:"POST",
+         headers:{
+            'Content-type':"Application/JSON"
+         },
+         body:JSON.stringify(postObj)
+      })
+      .then(res=>res.json())
+      .then(data=>{
+
+         let item=listItemGenerator(data)
+         posts.appendChild(item) 
+         this.reset() 
+
+
+         
+      })
+      .catch(e=>{
+         console.log(e.message)
+      })
+   }else{
+      alert("Please Provide Every details")
+   }
+
+})
+
+
+function listItemGenerator(item){
+   let li=document.createElement('li')
+   li.className='list-group-item'
+   li.innerHTML=` ${item.id} ${item.title} By User id ${item.userId}`
+   return li
+}
+
+
+
+
+
+/* function listItemGenerator(item,no){
+   let li=document.createElement('li')
+   li.className='list-group-item'
+   li.innerHTML=`${no} ${item.title}`
+   return li
+}
+ */
