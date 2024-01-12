4aeac2047f3d1412a0c755e4a9102b84144f9b5d
Eyasin81 <eyasina81@gmail.com>
2024-01-12 07:13:07 +0600

    Js Basic Number

--- 029JSForLoop.js
+++ 029JSForLoop.js
@@ -8,10 +8,13 @@
 // }
 // example-3 
 // 1-100 odd number output
-for(var i=1;i<100;i++){
-    if(i%2==0){
-        console.log(i)
-    }
-}
+// for(var i=1;i<100;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
 
+for(var i=0;i<100;i++){
+    console.log(`${i} agar kothai`)
+}
 
---
+++ 08 JS Basic Numbers.js
@@ -0,0 +1,37 @@
+var n=1451;
+var f=3.14;
+var  nn=Number("45.45");
+console.log(nn)
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+// ===============================
+// Number EPSILON
+const interval=Number.EPSILON
+console.log(`Number EPSION IS :${interval}`) //2.220446049250313e-16
+// Maximum Number of...
+const maxNumber=Number.MAX_VALUE
+console.log(`My Max Number is :${maxNumber}`)// 1.7976931348623157e+308
+
+// Minimum Number of...
+const miniNumber=Number.MIN_VALUE
+console.log(`My Min Number is :${miniNumber}`)// 1.7976931348623157e+308
+// Maximum Safe Integer 
+const MaximumSafeIntegerNumber=Number.MAX_SAFE_INTEGER
+console.log(`My Maximum Safe Integer Number is :${MaximumSafeIntegerNumber}`)
+// Undefine
+
+// Mini Safe Integer 
+const MinimumSafeInteger=Number.MIN_SAFE_INTEGER
+console.log(`My Minimum Safe Integer is :${MinimumSafeInteger}`)
+
+// Positive Maximum Number
+const PositiveMaximumNumber=Number.MAX_POSITIVE_INFINITY
+console.log(`Positive Maximum Number is :${PositiveMaximumNumber}`)
+// Negative Maximum Number
+const NegativeMaximumNumber=Number.MAX_NEGATIVE_INFINITY
+console.log(`Positive Maximum Number is :${NegativeMaximumNumber}`)
+
+// isNaN()
+let num = 123.456;
+console.log(num.toPrecision(4));  // ফলাফল: "123.5"
