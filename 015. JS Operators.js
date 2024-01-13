5ebb1a97baecb89af341abb5767804d6faeb1765
Eyasin81 <eyasina81@gmail.com>
2024-01-13 19:17:44 +0600

    15 js Operator List

---
+++ 015. JS Operators.js
@@ -0,0 +1,8 @@
+// 15.1 Arithmetic Operators 
+// 15.2  Assignment Operators
+//15.3  Comparison Operations    
+// 15.4 Logical Operators
+// 15.5 Bitwise Operators:
+// 15.6  Conditional (Ternary) Operator:
+// 15.7 Type Operators:
+// 15.8 Other Operators:
