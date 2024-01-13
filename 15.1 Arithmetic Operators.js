fc2e8b0d41bcfcc954faa3b6d606e829a0e262b1
Eyasin81 <eyasina81@gmail.com>
2024-01-13 19:19:39 +0600

    15.1 Arithmetic Operators

---
+++ 15.1 Arithmetic Operators.js
@@ -0,0 +1,38 @@
+// Arithmetic Operators 
+
+// Addition
+
+let a=12
+let b=10
+let c=a+b
+
+console.log(`Addition Result ${c}`) // 22
+// Subtraction
+let d=a-b 
+console.log(`Subtraction Result ${d}`)// 2
+
+// Multiplication
+let e=a*b
+console.log(`Multiplication Result: ${e}`)//120
+// Exponentiation (ES2016)
+
+let f=a+b*2
+console.log(`Exponentiation Result: ${f}`)// 32
+
+// Division
+
+let g=a/b  
+console.log(`Division Result: ${g}`)// 1.2
+// Modulus (Remainder)
+let h=a%b
+console.log(`Modulus Result: ${h}`)//2
+
+// Increment
+
+let j=20
+++j;
+console.log(`Increment Result: ${j}`)
+// Decrement 
+let k=20
+--k;
+console.log(`Increment Result: ${k}`)
